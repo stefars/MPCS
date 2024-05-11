55c7704e198becf36cecf93d2a5c1e3801e00b87
stefars <91747404+stefars@users.noreply.github.com>
2024-05-12 02:59:29 +0300

    Update SortingAlgorithms.py

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -134,49 +134,33 @@ def quicksort_reverse(list):
             else:
                 right.append(list[i])
         return quicksort_(left) + [pivot] + quicksort_(right[:-1])
-
-
-# Python program for implementation of MergeSort
-
-
+        
 def mergesort(arr):
     if len(arr) > 1:
-
-        # Finding the mid of the array
         mid = len(arr) // 2
+        left = arr[:mid]
 
-        # Dividing the array elements
-        L = arr[:mid]
-
-        # Into 2 halves
-        R = arr[mid:]
-
-        # Sorting the first half
-        mergesort(L)
-
-        # Sorting the second half
-        mergesort(R)
-
+        right = arr[mid:]
+        
+        mergesort(left)
+        mergesort(right)
+        
         i = j = k = 0
-
-        # Copy data to temp arrays L[] and R[]
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                arr[k] = L[i]
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
                 i += 1
             else:
-                arr[k] = R[j]
+                arr[k] = right[j]
                 j += 1
             k += 1
-
-        # Checking if any element was left
-        while i < len(L):
-            arr[k] = L[i]
+        while i < len(left):
+            arr[k] = left[i]
             i += 1
             k += 1
 
-        while j < len(R):
-            arr[k] = R[j]
+        while j < len(right):
+            arr[k] = right[j]
             j += 1
             k += 1
     return arr
